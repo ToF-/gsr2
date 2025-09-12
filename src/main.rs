3d709c724627d68f22062b7751c1899991db91e8
Christophe Thibaut <cthibauttof@gmail.com>
2025-09-12 19:53:13 +0200

    checking that supplied path exists

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
+use std::process::exit;
 use crate::command_line_interface::CommandLineInterface;
 
 mod command_line_interface;
 mod paths;
 
 fn main() {
-    let cli = CommandLineInterface::parse_and_check().unwrap();
-    match cli.directory {
-        Some(dir) => println!("directory: {}", dir),
-        None => println!("no directory supplied"),
-    }
+    let cli = match CommandLineInterface::parse_and_check() {
+        Ok(cli) => match cli.directory {
+            Some(dir) => println!("directory: {}", dir),
+            None => println!("no directory supplied"),
+        },
+        Err(err) => {
+            eprintln!("{}", err);
+            exit(1);
+        },
+    };
+    exit(0);
 }
