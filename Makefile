95bdd3d67110cc0c2b18d5003f42c7df2485021b
Christophe Thibaut <cthibauttof@gmail.com>
2025-09-20 09:33:39 +0200

    insert test data sql script

--- Makefile
+++ Makefile
@@ -1,3 +1,5 @@
 create_db: sql/create_db.sql
 	sqlite3 testdata/gsr2.db <sql/create_db.sql
 	sqlite3 testdata/gsr2.db ".schema"
+	sqlite3 testdata/gsr2.db <sql/insert_test_data.sql
+	sqlite3 testdata/gsr2.db "SELECT * FROM Picture ORDER BY FilePath;"
---
+++ sql/insert_test_data.sql
@@ -0,0 +1,11 @@
+DELETE FROM Picture;
+
+INSERT INTO Picture (FilePath, Label)
+VALUES ("testdata/nine_colors.png", "sample");
+
+INSERT INTO Picture (FilePath, Label)
+VALUES ("testdata/single_dot.png", "");
+
+INSERT INTO Picture (FilePath, Label)
+VALUES ("testdata/white_square.png", "foo");
+
