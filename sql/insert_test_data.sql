afa76db2024958961a0b92c46cd241973608474d
Christophe Thibaut <cthibauttof@gmail.com>
2025-09-20 10:25:12 +0200

    fixed syntax in sql script

--- sql/insert_test_data.sql
+++ sql/insert_test_data.sql
@@ -1,11 +1,11 @@
 DELETE FROM Picture;
 
 INSERT INTO Picture (FilePath, Label)
-VALUES ("testdata/nine_colors.png", "sample");
+VALUES ('testdata/nine_colors.png', 'sample');
 
 INSERT INTO Picture (FilePath, Label)
-VALUES ("testdata/single_dot.png", "");
+VALUES ('testdata/single_dot.png', '');
 
 INSERT INTO Picture (FilePath, Label)
-VALUES ("testdata/white_square.png", "foo");
+VALUES ('testdata/white_square.png', 'foo');
 
