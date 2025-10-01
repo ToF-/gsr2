a616c9353988f741a4b46a6023fc09688ad0f768
Christophe Thibaut <cthibauttof@gmail.com>
2025-10-01 22:38:59 +0200

    database schema

--- sql/create_db.sql
+++ sql/create_db.sql
@@ -1,4 +1,16 @@
 CREATE TABLE IF NOT EXISTS Picture (
     FilePath TEXT NOT NULL PRIMARY KEY,
-    Label TEXT NOT NULL
-    )
+    Label TEXT NOT NULL,
+    FileSize INTEGER,
+    ModifiedTime INTEGER,
+    Rank INTEGER,
+    Palette BLOB,
+    Selected BOOLEAN,
+    Deleted BOOLEAN,
+    Cover BOOLEAN);
+
+CREATE TABLE IF NOT EXISTS Tag (
+    FilePath TEXT NOT NULL,
+    Label TEXT NOT NULL,
+    PRIMARY KEY (FilePath, Label));
+
