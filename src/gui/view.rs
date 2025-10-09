315b5f4def825acb0b04fd989ccf556e50535634
Christophe Thibaut <cthibauttof@gmail.com>
2025-10-10 00:16:19 +0200

    entry window wip

--- src/gui/view.rs
+++ src/gui/view.rs
@@ -3,3 +3,4 @@ pub mod main_window;
 pub mod picture_cell_box;
 pub mod picture_frame;
 pub mod picture_grid;
+pub mod entry_window;
---
+++ src/gui/view/entry_window.rs
@@ -0,0 +1,63 @@
+use gtk::Align;
+use crate::env::default_values::{ENTRY_WINDOW_HEIGHT, ENTRY_WINDOW_WIDTH};
+
+use gtk::Orientation;
+#[derive(Clone,Debug)]
+pub struct EntryWindow {
+    entry_window: gtk::Window,
+    digits_only: bool,
+}
+
+impl EntryWindow {
+    pub fn new(application_window: &gtk::ApplicationWindow, prompt: &str, text: &str, digits_only: bool) -> Self {
+        let entry_text = gtk::Label::builder()
+            .valign(Align::Center)
+            .halign(Align::Center)
+            .label(text)
+            .build();
+        let prompt_label = gtk::Label::builder()
+            .valign(Align::Center)
+            .halign(Align::Center)
+            .label(prompt)
+            .build();
+        let entry_box = gtk::Box::builder()
+            .orientation(Orientation::Vertical)
+            .spacing(0)
+            .halign(Align::Fill)
+            .valign(Align::Fill)
+            .hexpand(true)
+            .vexpand(true)
+            .homogeneous(false)
+            .build();
+        entry_box.add_child(prompt_label);
+        entry_box.add_child(entry_text);
+        let entry_window = gtk::Window::builder()
+            .width(ENTRY_WINDOW_WIDTH)
+            .height(ENTRY_WINDOW_HEIGHT)
+            .tansient_for(application_window)
+            .build();
+        entry_window.set_child(&entry_box);
+        EntryWindow {
+            entry_window: entry_window,
+            digits_only,
+        }
+    }
+
+    pub fn entry_text(&self) -> String {
+        let label = self.entry_window.first_child()
+            .unwrap().expect("can't get entry window's child")
+            .downcast::<gtk::Box>().expect("can't downcast as box")
+            .first_child()
+            .unwrap().expect("can't get entry prompt")
+            .downcast::<gtk::Label>().expect("can't downcast as label")
+            .next_sibling()
+            .unwrap().expect("can't get next label")
+            .downcast::<gtk::Label>().expect("can't downcast as label")
+            .label();
+        label
+    }
+}
+
+
+
+
