8fa95dd8fa1b52a1d44a6567db9c9aad4f5d342e
Christophe Thibaut <cthibauttof@gmail.com>
2025-10-14 22:14:44 +0200

    changed unicode for focus

--- src/gui/display.rs
+++ src/gui/display.rs
@@ -28,7 +28,7 @@ fn page_display(controller: &Controller) -> String {
 }
 pub fn picture_label_display(label: &str, with_focus: Option<char>) -> String {
     format!(
-        "{}{}",
+        "{} {}",
         if let Some(symbol) = with_focus {
             symbol
         } else {
