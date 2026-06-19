fca69974981a83aa9c65d809374f6cc28b3f8ee6
ToF- <cthibauttof@gmail.com>
2026-06-20 00:09:32 +0200

    h toggles single view

--- src/gui/control.rs
+++ src/gui/control.rs
@@ -196,6 +196,10 @@ pub fn default_controls() -> Controls {
             Control::ToggleSingleView,
         ),
         (
+            (String::from("h"), Mode::View),
+            Control::ToggleSingleView,
+        ),
+        (
             (String::from("dead_circumflex"), Mode::View),
             Control::ToggleSingleView,
         ),
