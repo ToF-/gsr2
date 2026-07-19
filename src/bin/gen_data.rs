b94e9c67f6dc1a8900b995a36b99872397daf385
pinage404 <pinage404@gmail.com>
2026-07-19 23:51:30 +0200

    WIP

---
+++ src/bin/gen_data.rs
@@ -0,0 +1,29 @@
+extern crate image;
+use gsr::test_data::*;
+
+pub fn save_large_picture() {
+    let image = large_picture();
+    image.save(large_picture_file_path()).unwrap();
+}
+
+pub fn save_nine_colors() {
+    let image = gen_nine_colors();
+    image.save(nine_colors_file_path()).unwrap();
+}
+
+pub fn save_single_dot() {
+    let image = gen_single_dot();
+    image.save(single_dot_file_path()).unwrap();
+}
+
+pub fn save_white_square() {
+    let image = gen_white_square();
+    image.save(white_square_file_path()).unwrap();
+}
+
+pub fn main() {
+    save_large_picture();
+    save_nine_colors();
+    save_single_dot();
+    save_white_square();
+}
