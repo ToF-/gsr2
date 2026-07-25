673eacd77c676bdfd6615765c61b1580316992f7
Christophe Thibaut <cthibauttof@gmail.com>
2026-07-25 18:45:11 +0200

    Change T)ag instead of A)dd tag

--- src/gui/editor.rs
+++ src/gui/editor.rs
@@ -45,7 +45,7 @@ impl Editor {
         let prompt: &str = match entry_kind {
             EntryKind::Rename => { "Enter a name for the selected picture" }
             EntryKind::Change => {
-                "Enter what to change: A)dd tag C)ategory L)abel R)emove tag U)nlabel co(V)er on"
+                "Enter what to change: C)ategory L)abel R)emove T)ag U)nlabel co(V)er on"
             }
             EntryKind::Label => "Enter a label",
             EntryKind::AddTag => "Enter a new tag to add",
@@ -201,7 +201,7 @@ impl Editor {
         };
         let ch_is_ok = match self.entry_kind {
             EntryKind::Change => {
-                matches!(ch, 'a' | 'c' | 'l' | 'r' | 'u' | 'v' )
+                matches!(ch, 'c' | 'l' | 'r' | 't' | 'u' | 'v' )
             }
             EntryKind::Number => ch.is_ascii_digit(),
             EntryKind::DeleteConfirmation
@@ -282,11 +282,11 @@ impl Editor {
             }
             c if self.entry_kind == EntryKind::Change => {
                 let change = match c {
-                    'a' => "AddTag",
                     'c' => "Category",
                     'l' => "Label",
                     'n' => "Name",
                     'r' => "RemoveTag",
+                    't' => "AddTag",
                     'u' => "Unlabel",
                     'v' => "Cover",
                     _ => todo!(),
