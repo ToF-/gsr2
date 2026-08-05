089d6c3d1fa46f32b54e0607d56a171047827c74
ToF- <cthibauttof@gmail.com>
2026-08-05 22:45:14 +0200

    main controller is now an glib object wrapper for controller

---
+++ src/gui/main_controller/imp.rs
@@ -0,0 +1,55 @@
+use crate::gui::controller::Controller;
+use gtk::gio::ActionEntry;
+use gtk::gio::prelude::*;
+use gtk::glib;
+use gtk::glib::clone;
+use gtk::glib::subclass::Signal;
+use gtk::glib::subclass::prelude::*;
+use std::cell::RefCell;
+use std::sync::OnceLock;
+
+pub struct MainController {
+    pub actions: gtk::gio::SimpleActionGroup,
+    controller_opt_rc : RefCell<Option<Controller>>, 
+}
+
+impl Default for MainController {
+    fn default() -> Self {
+        Self {
+            actions: gtk::gio::SimpleActionGroup::new(),
+            controller_opt_rc : RefCell::new(None),
+        }
+    }
+}
+
+#[gtk::glib::object_subclass]
+impl ObjectSubclass for MainController {
+    const NAME: &'static str = "MainController";
+    type Type = super::MainController;
+    type ParentType = gtk::glib::Object;
+}
+
+impl ObjectImpl for MainController {
+    fn signals() -> &'static [Signal] {
+        static SIGNALS: OnceLock<Vec<Signal>> = OnceLock::new();
+
+        SIGNALS.get_or_init(|| {
+            vec![
+                Signal::builder("closed").build(),
+                Signal::builder("action").build(),
+            ]
+        })
+    }
+}
+
+impl MainController {
+    pub fn new(controller_opt: Option<Controller>) -> Self {
+        let obj = Self::default();
+        obj.initialize(controller_opt);
+        obj
+    }
+
+    pub fn initialize(&self, controller_opt: Option<Controller>) {
+        *self.controller_opt_rc.borrow_mut() = controller_opt
+    }
+}
---
+++ src/gui/main_controller/mod.rs
@@ -0,0 +1,39 @@
+use crate::gui::controller::Controller;
+use gtk::prelude::ObjectExt;
+use gtk::subclass::prelude::ObjectSubclassIsExt;
+use std::cell::RefCell;
+use std::rc::Rc;
+
+mod imp;
+pub type RcMainController = Rc<RefCell<MainController>>;
+
+gtk::glib::wrapper! {
+    pub struct MainController(ObjectSubclass<imp::MainController>);
+}
+
+impl MainController {
+    pub fn new(controller_opt: Option<Controller>) -> Self {
+        let obj: Self = gtk::glib::Object::new();
+        obj.imp().initialize(controller_opt);
+        obj
+    }
+
+    pub fn actions(&self) -> gtk::gio::SimpleActionGroup {
+        self.imp().actions.clone()
+    }
+
+    pub fn close(&self) {
+        self.emit_by_name::<()>("closed", &[]);
+    }
+
+    pub fn connect_closed<F>(&self, f: F) -> gtk::glib::SignalHandlerId
+    where
+        F: Fn(&MainController) + 'static,
+    {
+        self.connect_local("closed", false, move |values| {
+            let obj = values[0].get::<MainController>().unwrap();
+            f(&obj);
+            None
+        })
+    }
+}
