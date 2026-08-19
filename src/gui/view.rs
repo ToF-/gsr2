cd546921379a322c96b3ca42514d1a000b986447
ToF- <cthibauttof@gmail.com>
2026-08-19 10:12:04 +0200

    toggle view state

--- src/gui/view.rs
+++ src/gui/view.rs
@@ -7,6 +7,7 @@ pub struct View {
     pictures_per_row: i32,
     last_pictures_per_row: i32,
     palette_on: bool,
+    full_size: bool,
 }
 
 impl Default for View {
@@ -15,6 +16,7 @@ impl Default for View {
            pictures_per_row: 10,
            last_pictures_per_row: 1,
            palette_on: false,
+           full_size: false,
         }
     }
 }
@@ -31,10 +33,36 @@ impl View {
 
     pub fn toggle_pictures_per_row(&mut self) {
         std::mem::swap(&mut self.pictures_per_row,&mut self.last_pictures_per_row);
+        self.full_size = false;
     }
     
     pub fn palette_on(&self) -> bool {
         self.palette_on
+    }
+
+    pub fn toggle_palette_on(&mut self) {
+        self.palette_on = ! self.palette_on
+    }
+
+    pub fn full_size(&self) -> bool {
+        self.full_size
+    }
+
+    pub fn toggle_full_size(&mut self) -> bool {
+        if self.single_view() {
+            self.full_size = !self.full_size;
+            true
+        } else {
+            false
+        }
+    }
+
+    pub fn single_view(&self) -> bool {
+        self.pictures_per_row == 1
+    }
+
+    pub fn thumbnail_view(&self) -> bool {
+        self.pictures_per_row == 10
     }
 }
 
@@ -47,6 +75,7 @@ mod tests {
         let view = View::default();
         assert_eq!(10, view.pictures_per_row());
         assert_eq!(false, view.palette_on());
+        assert_eq!(false, view.full_size());
         
     }
 
@@ -63,6 +92,30 @@ mod tests {
         assert_eq!(5, view.pictures_per_row());
         view.toggle_pictures_per_row();
         assert_eq!(2, view.pictures_per_row());
+    }
 
+    #[test]
+    fn switching_palette_on() {
+        let mut view = View::default();
+        view.toggle_palette_on();
+        assert!(view.palette_on());
+        view.toggle_palette_on();
+        assert!(!view.palette_on());
+    }
+
+    #[test]
+    fn switching_full_size_only_when_single_view() {
+        let mut view = View::default();
+        assert!(! view.single_view());
+        assert!(view.thumbnail_view());
+        assert!(! view.toggle_full_size());
+        assert!(! view.full_size());
+        view.set_pictures_per_row(1);
+        assert!(view.single_view());
+        assert!(view.toggle_full_size());
+        assert!(view.full_size());
+        view.toggle_pictures_per_row();
+        assert!(! view.full_size());
+            
     }
 }
